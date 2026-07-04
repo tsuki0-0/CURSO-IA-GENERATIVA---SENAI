22228fe1ef684b319fa80eb23d3595c069fcd1e2
Tsuki <jhuansilvarrjkm@gmail.com>
2026-07-04 17:57:08 -0300

    Create Atividade Prompt.py

---
+++ Atividades do Curso/Aula 3 - 4/Atividade Prompt.py
@@ -0,0 +1,73 @@
+# Atividade 1 – Comparação entre prompts Tema sugerido: **Marketing Digital**
+# Prompt Simples
+# Crie um prompt simples.
+# Exemplo esperado:
+# Explique o que é marketing digital.
+
+# Prompt RTF
+# Estrutura com Exemplo:
+# Role: Você é especialista em marketing digital.
+# Task: Explique o que é marketing digital.
+# Format: Responda em uma lista com 3 tópicos.
+
+# Prompt CRAFT
+# Estrutura com Exemplo:
+# Contexto: Uma pequena empresa quer melhorar suas vendas online.
+# Role: Você é especialista em marketing digital.
+# Action: Explique estratégias de marketing digital.
+# Format: Apresente 5 estratégias em lista. formule em Markdown
+# Target: Empreendedores iniciantes.
+
+# Prompt COSTAR
+# Estrutura com Exemplo:
+# Contexto: Uma startup deseja crescer nas redes sociais.
+# Objetivo: Criar estratégias de marketing digital.
+# Estilo: Didático.
+# Tom: Profissional.
+# Audiência: Empreendedores iniciantes.
+# Resposta: Liste 5 estratégias e explique cada uma.
+
+# Atividade 2 – Identificação de estrutura
+# Pergunta - Identifique qual estrutura está sendo utilizada:
+
+# Prompt A: Prompt Simples
+# Explique o que é computação em nuvem.
+
+# Prompt B: R.T.F
+# Role: Você é professor de tecnologia.
+
+# Task: Explique computação em nuvem.
+
+# Format: Responda em 3 tópicos
+
+# Prompt C: C.R.A.F.T
+# Contexto: Uma empresa quer migrar seus sistemas para a nuvem.
+
+# Role: Especialista em cloud computing.
+
+# Action: Explique vantagens da computação em nuvem.
+
+# Format: Lista de 5 pontos.
+
+# Target: Gestores de TI iniciantes.
+
+# Prompt D: C.O.S.T.A.R
+# Contexto: Uma empresa quer migrar para a nuvem.
+
+# Objetivo: Explicar benefícios da computação em nuvem.
+
+# Estilo: Educativo.
+
+# Tom: Profissional.
+
+# Audiência: Gestores de TI.
+
+# Formato da resposta: 5 tópicos explicados.
+
+# Atividade 3 – Criação de prompt profissional
+# - Utilizar esses dados abaixo:
+# - uma campanha de divulgação
+# - voltada para jovens
+# - com linguagem simples
+# - em formato de texto para redes sociais
+# Prompt
